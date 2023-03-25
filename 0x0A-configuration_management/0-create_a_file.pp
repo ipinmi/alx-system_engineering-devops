09f803536014bc9a1b64124ac9eda479588d9cad
ipinmi <ipinmiadebayo@gmail.com>
2023-03-25 15:31:19 -0700

    tasks 0-2

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
+}
